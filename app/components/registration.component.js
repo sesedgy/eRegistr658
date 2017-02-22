769b5566783642e127383c10f572a12bebb5aac4
MLukanin <XSW@zaq1>
2017-02-22 17:42:15 +0300

    create validation

--- app/components/registration.component.js
+++ app/components/registration.component.js
@@ -11,17 +11,33 @@ System.register(['@angular/core'], function(exports_1, context_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var RegistrationComponent;
+    var Abiturient, User, RegistrationComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
+            Abiturient = (function () {
+                function Abiturient() {
+                }
+                return Abiturient;
+            }());
+            exports_1("Abiturient", Abiturient);
+            User = (function () {
+                function User() {
+                }
+                return User;
+            }());
+            exports_1("User", User);
             RegistrationComponent = (function () {
                 function RegistrationComponent() {
+                    this.abiturient = new Abiturient();
                     this.firstStageVisible = false;
                     this.secondStageVisible = true;
+                    this.thirdStageVisible = true;
+                    this.fourthStageVisible = true;
+                    this.fifthStageVisible = true;
                     this.chapter = "1";
                 }
                 RegistrationComponent.prototype.firstStageNext = function () {
@@ -29,16 +45,53 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                     this.secondStageVisible = false;
                     this.chapter = "2";
                 };
+                RegistrationComponent.prototype.firstStageSmallCard = function () {
+                    //TODO Предварительная заявка
+                };
                 RegistrationComponent.prototype.secondStagePrev = function () {
                     this.firstStageVisible = false;
                     this.secondStageVisible = true;
                     this.chapter = "1";
                 };
+                RegistrationComponent.prototype.secondStageNext = function () {
+                    this.secondStageVisible = true;
+                    this.thirdStageVisible = false;
+                    this.chapter = "3";
+                };
+                RegistrationComponent.prototype.thirdStagePrev = function () {
+                    this.secondStageVisible = false;
+                    this.thirdStageVisible = true;
+                    this.chapter = "2";
+                };
+                RegistrationComponent.prototype.thirdStageNext = function () {
+                    this.thirdStageVisible = true;
+                    this.fourthStageVisible = false;
+                    this.chapter = "4";
+                };
+                RegistrationComponent.prototype.fourthStagePrev = function () {
+                    this.thirdStageVisible = false;
+                    this.fourthStageVisible = true;
+                    this.chapter = "3";
+                };
+                RegistrationComponent.prototype.fourthStageNext = function () {
+                    this.fourthStageVisible = true;
+                    this.fifthStageVisible = false;
+                    this.chapter = "5";
+                };
+                RegistrationComponent.prototype.fifthStagePrev = function () {
+                    this.fourthStageVisible = false;
+                    this.fifthStageVisible = true;
+                    this.chapter = "4";
+                };
+                RegistrationComponent.prototype.finishRegistration = function () {
+                    this.firstStageVisible = true;
+                    this.secondStageVisible = false;
+                };
                 RegistrationComponent = __decorate([
                     core_1.Component({
                         selector: 'registration',
                         templateUrl: 'app/views/registration.html',
-                        styles: ["\n    .row{margin-right: 0px; margin-left: 0px;}\n    "]
+                        styles: ["\n    .row{margin-right: 0px; margin-left: 0px;}\n    input.ng-touched.ng-invalid {border:solid red 2px;}\n    input.ng-touched.ng-valid {border:solid green 2px;}\n    select.ng-touched.ng-invalid {border:solid red 2px;}\n    select.ng-touched.ng-valid {border:solid green 2px;}\n    "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], RegistrationComponent);
--- app/components/registration.component.ts
+++ app/components/registration.component.ts
@@ -1,18 +1,57 @@
 import { Component} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import {FormsModule, NgModel}   from '@angular/forms';
+
+export class Abiturient{
+    public lastName: string;
+    public citizenship: string;
+    public birthDate: string;
+    public firstName: string;
+    public sex: string;
+    public mobilePhone: string;
+    public middleName: string;
+    public nation: string;
+    public specialty: string;
+    public formOfEducation: string;
+    public qualification: string;
+    public email: string;
+    public typeStudentDocument: string;
+    public passportSeries: string;
+    public passportNumber: string;
+    public passportDate: string;
+    public passportIssueOrg: string;
+    public countryRegistration: string;
+    public cityRegistration: string;
+    public regionRegistration: string;
+    public districtRegistration: string;
+    public locality: string;
+}
+
+export class User{
+    public title: string;
+    public price: number;
+    public company: string;
+}
 
 @Component({
     selector: 'registration',
     templateUrl: 'app/views/registration.html',
     styles:[`
     .row{margin-right: 0px; margin-left: 0px;}
+    input.ng-touched.ng-invalid {border:solid red 2px;}
+    input.ng-touched.ng-valid {border:solid green 2px;}
+    select.ng-touched.ng-invalid {border:solid red 2px;}
+    select.ng-touched.ng-valid {border:solid green 2px;}
     `]
 })
 export class RegistrationComponent {
 
+    abiturient: Abiturient = new Abiturient();
     firstStageVisible: boolean = false;
     secondStageVisible: boolean = true;
+    thirdStageVisible: boolean = true;
+    fourthStageVisible: boolean = true;
+    fifthStageVisible: boolean = true;
     chapter: string = "1"
 
     firstStageNext(){
@@ -20,9 +59,47 @@ export class RegistrationComponent {
         this.secondStageVisible =  false;
         this.chapter = "2";
     }
+    firstStageSmallCard(){
+    //TODO Предварительная заявка
+    }
     secondStagePrev(){
         this.firstStageVisible =  false;
         this.secondStageVisible =  true;
         this.chapter = "1";
     }
+    secondStageNext(){
+        this.secondStageVisible =  true;
+        this.thirdStageVisible =  false;
+        this.chapter = "3";
+    }
+    thirdStagePrev(){
+        this.secondStageVisible =  false;
+        this.thirdStageVisible =  true;
+        this.chapter = "2";
+    }
+    thirdStageNext(){
+        this.thirdStageVisible =  true;
+        this.fourthStageVisible =  false;
+        this.chapter = "4";
+    }
+    fourthStagePrev(){
+        this.thirdStageVisible =  false;
+        this.fourthStageVisible =  true;
+        this.chapter = "3";
+    }
+    fourthStageNext(){
+        this.fourthStageVisible =  true;
+        this.fifthStageVisible =  false;
+        this.chapter = "5";
+    }
+    fifthStagePrev(){
+        this.fourthStageVisible =  false;
+        this.fifthStageVisible =  true;
+        this.chapter = "4";
+    }
+    finishRegistration(){
+        this.firstStageVisible =  true;
+        this.secondStageVisible =  false;
+    }
+
 }
