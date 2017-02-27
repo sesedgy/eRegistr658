b0fa313be68723e62d6e15cbead2e6c544d20e30
MLukanin <XSW@zaq1>
2017-02-27 19:13:20 +0300

    update RegistrationForm

--- app/components/registration.component.js
+++ app/components/registration.component.js
@@ -89,18 +89,67 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                     this.chapter = "4";
                 };
                 RegistrationComponent.prototype.finishRegistration = function () {
-                    this.firstStageVisible = true;
-                    this.secondStageVisible = false;
+                    //TODO Отправить заявку заявка
                 };
-                RegistrationComponent.prototype.adressLiveCheckBox = function () {
+                RegistrationComponent.prototype.adressLiveCheckBoxFunction = function ($event) {
+                    if ($event.target.checked) {
+                        this.abiturient.countryLive = this.abiturient.countryRegistration;
+                        this.abiturient.cityLive = this.abiturient.cityRegistration;
+                        this.abiturient.regionLive = this.abiturient.regionRegistration;
+                        this.abiturient.districtLive = this.abiturient.districtRegistration;
+                        this.abiturient.localityLive = this.abiturient.localityRegistration;
+                        this.abiturient.streetLive = this.abiturient.streetRegistration;
+                        this.abiturient.houseLive = this.abiturient.houseRegistration;
+                        this.abiturient.buildingLive = this.abiturient.buildingRegistration;
+                        this.abiturient.housingLive = this.abiturient.housingRegistration;
+                        this.abiturient.flatLive = this.abiturient.flatRegistration;
+                        this.abiturient.indexLive = this.abiturient.indexRegistration;
+                    }
+                    else {
+                        this.abiturient.countryLive = "";
+                        this.abiturient.cityLive = "";
+                        this.abiturient.regionLive = "";
+                        this.abiturient.districtLive = "";
+                        this.abiturient.localityLive = "";
+                        this.abiturient.streetLive = "";
+                        this.abiturient.houseLive = "";
+                        this.abiturient.buildingLive = "";
+                        this.abiturient.housingLive = "";
+                        this.abiturient.flatLive = "";
+                        this.abiturient.indexLive = "";
+                    }
                 };
-                RegistrationComponent.prototype.adressCustomerCheckBox = function () {
+                RegistrationComponent.prototype.adressCustomerCheckBoxFunction = function ($event) {
+                    if ($event.target.checked) {
+                        this.abiturient.countryCustomer = this.abiturient.countryRegistration;
+                        this.abiturient.cityCustomer = this.abiturient.cityRegistration;
+                        this.abiturient.regionCustomer = this.abiturient.regionRegistration;
+                        this.abiturient.districtCustomer = this.abiturient.districtRegistration;
+                        this.abiturient.localityCustomer = this.abiturient.localityRegistration;
+                        this.abiturient.streetCustomer = this.abiturient.streetRegistration;
+                        this.abiturient.houseCustomer = this.abiturient.houseRegistration;
+                        this.abiturient.buildingCustomer = this.abiturient.buildingRegistration;
+                        this.abiturient.housingCustomer = this.abiturient.housingRegistration;
+                        this.abiturient.flatCustomer = this.abiturient.flatRegistration;
+                    }
+                    else {
+                        this.abiturient.countryCustomer = "";
+                        this.abiturient.cityCustomer = "";
+                        this.abiturient.regionCustomer = "";
+                        this.abiturient.districtCustomer = "";
+                        this.abiturient.localityCustomer = "";
+                        this.abiturient.streetCustomer = "";
+                        this.abiturient.houseCustomer = "";
+                        this.abiturient.buildingCustomer = "";
+                        this.abiturient.housingCustomer = "";
+                        this.abiturient.flatCustomer = "";
+                    }
                 };
                 RegistrationComponent = __decorate([
                     core_1.Component({
                         selector: 'registration',
                         templateUrl: 'app/views/registration.html',
-                        styles: ["\n    .row{margin-right: 0px; margin-left: 0px;}\n    input.ng-touched.ng-invalid {border:solid red 2px;}\n    input.ng-touched.ng-valid {border:solid green 2px;}\n    select.ng-touched.ng-invalid {border:solid red 2px;}\n    select.ng-touched.ng-valid {border:solid green 2px;}\n    "]
+                        styles: ["\n    .formRegistration {\n        height:620px;\n    }\n    @media (max-width: 1200px){\n    .formRegistration {\n        height:initial;\n    }}  \n    .row{margin-right: 0px; margin-left: 0px;}\n    input.ng-touched.ng-invalid {border:solid red 2px;}\n    input.ng-touched.ng-valid {border:solid green 2px;}\n    select.ng-touched.ng-invalid {border:solid red 2px;}\n    select.ng-touched.ng-valid {border:solid green 2px;}\n    "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], RegistrationComponent);
--- app/components/registration.component.ts
+++ app/components/registration.component.ts
@@ -6,12 +6,12 @@ import {FormsModule, NgModel}   from '@angular/forms';
 
 export class Abiturient{
     public lastName: string;
-    public citizenship: string;
-    public birthDate: string;
     public firstName: string;
-    public sex: string;
-    public mobilePhone: string;
     public middleName: string;
+    public birthDate: string;
+    public mobilePhone: string;
+    public citizenship: string;
+    public sex: string;
     public nation: string;
     public specialty: string;
     public formOfEducation: string;
@@ -33,18 +33,80 @@ export class Abiturient{
     public housingRegistration: string;
     public flatRegistration: string;
     public indexRegistration: string;
+    public countryLive: string;
+    public cityLive: string;
+    public regionLive: string;
+    public districtLive: string;
+    public localityLive: string;
+    public streetLive: string;
+    public houseLive: string;
+    public buildingLive: string;
+    public housingLive: string;
+    public flatLive: string;
+    public indexLive: string;
+    public lastNameMother: string;
+    public firstNameMother: string;
+    public middleNameMother: string;
+    public mobilePhoneMother: string;
+    public lastNameFather: string;
+    public firstNameFather: string;
+    public middleNameFather: string;
+    public mobilePhoneFather: string;
+    public levelEducation: string;
+    public seriesEducationDocument: string;
+    public numberEducationDocument: string;
+    public dateEducationDocument: string;
+    public whoGiveEducationDocument: string;
+    public uploadedFileName: string;
+    public paymentForm: string;
+    public lastNameCustomer: string;
+    public firstNameCustomer: string;
+    public middleNameCustomer: string;
+    public mobilePhoneCustomer: string;
+    public passportSeriesCustomer: string;
+    public passportNumberCustomer: string;
+    public passportDateCustomer: string;
+    public passportIssueOrgCustomer: string;
+    public countryCustomer: string;
+    public cityCustomer: string;
+    public regionCustomer: string;
+    public districtCustomer: string;
+    public localityCustomer: string;
+    public streetCustomer: string;
+    public houseCustomer: string;
+    public buildingCustomer: string;
+    public housingCustomer: string;
+    public flatCustomer: string;
+    public nameOfTheOrganization: string;
+    public INN: string;
+    public KPP: string;
+    public legalAddress: string;
+    public actualAddress: string;
+    public nameOfTheBank: string;
+    public INNOfTheBank: string;
+    public BICOfTheBank: string;
+    public checkingAccount: string;
+    public correspondingAccount: string;
+    public FIORepresentative: string;
+    public phoneOfTheOrganisation: string;
+
 }
 
 export class User{
-    public title: string;
-    public price: number;
-    public company: string;
+
 }
 
 @Component({
     selector: 'registration',
     templateUrl: 'app/views/registration.html',
     styles:[`
+    .formRegistration {
+        height:620px;
+    }
+    @media (max-width: 1200px){
+    .formRegistration {
+        height:initial;
+    }}  
     .row{margin-right: 0px; margin-left: 0px;}
     input.ng-touched.ng-invalid {border:solid red 2px;}
     input.ng-touched.ng-valid {border:solid green 2px;}
@@ -114,16 +176,63 @@ export class RegistrationComponent {
         this.chapter = "4";
     }
     finishRegistration(){
-        this.firstStageVisible =  true;
-        this.secondStageVisible =  false;
+        //TODO Отправить заявку заявка
     }
 
-    adressLiveCheckBox(){
-
+    adressLiveCheckBoxFunction($event){
+        if($event.target.checked){
+            this.abiturient.countryLive = this.abiturient.countryRegistration;
+            this.abiturient.cityLive = this.abiturient.cityRegistration;
+            this.abiturient.regionLive = this.abiturient.regionRegistration;
+            this.abiturient.districtLive = this.abiturient.districtRegistration;
+            this.abiturient.localityLive = this.abiturient.localityRegistration;
+            this.abiturient.streetLive = this.abiturient.streetRegistration;
+            this.abiturient.houseLive = this.abiturient.houseRegistration;
+            this.abiturient.buildingLive = this.abiturient.buildingRegistration;
+            this.abiturient.housingLive = this.abiturient.housingRegistration;
+            this.abiturient.flatLive = this.abiturient.flatRegistration;
+            this.abiturient.indexLive = this.abiturient.indexRegistration;
+        }
+        else{
+            this.abiturient.countryLive = "";
+            this.abiturient.cityLive = "";
+            this.abiturient.regionLive = "";
+            this.abiturient.districtLive = "";
+            this.abiturient.localityLive = "";
+            this.abiturient.streetLive = "";
+            this.abiturient.houseLive = "";
+            this.abiturient.buildingLive = "";
+            this.abiturient.housingLive = "";
+            this.abiturient.flatLive = "";
+            this.abiturient.indexLive = "";
+        }
     }
 
-    adressCustomerCheckBox(){
-
+    adressCustomerCheckBoxFunction($event){
+        if($event.target.checked){
+            this.abiturient.countryCustomer = this.abiturient.countryRegistration;
+            this.abiturient.cityCustomer = this.abiturient.cityRegistration;
+            this.abiturient.regionCustomer = this.abiturient.regionRegistration;
+            this.abiturient.districtCustomer = this.abiturient.districtRegistration;
+            this.abiturient.localityCustomer = this.abiturient.localityRegistration;
+            this.abiturient.streetCustomer = this.abiturient.streetRegistration;
+            this.abiturient.houseCustomer = this.abiturient.houseRegistration;
+            this.abiturient.buildingCustomer = this.abiturient.buildingRegistration;
+            this.abiturient.housingCustomer = this.abiturient.housingRegistration;
+            this.abiturient.flatCustomer = this.abiturient.flatRegistration;
+        }
+        else{
+            this.abiturient.countryCustomer = "";
+            this.abiturient.cityCustomer = "";
+            this.abiturient.regionCustomer = "";
+            this.abiturient.districtCustomer = "";
+            this.abiturient.localityCustomer = "";
+            this.abiturient.streetCustomer = "";
+            this.abiturient.houseCustomer = "";
+            this.abiturient.buildingCustomer = "";
+            this.abiturient.housingCustomer = "";
+            this.abiturient.flatCustomer = "";
+        }
     }
 
 }
